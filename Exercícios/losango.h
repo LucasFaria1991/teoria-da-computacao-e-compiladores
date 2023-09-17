911202c76b193b1afba37bb46dca3fa233979232
LucasFaria1991 <142852119+LucasFaria1991@users.noreply.github.com>
2023-09-17 12:47:45 -0300

    Exercício 1
    O programa principal (main.c) chama todos os código a partir do switch

---
+++ Exercícios/losango.h
@@ -0,0 +1,15 @@
+void losango(){
+
+float diagMaior, diagMenor, area;
+
+printf("Digite a diagonal maior do losango: ");
+scanf("%f", &diagMaior);
+
+printf("Digite a diagonal menor do losango: ");
+scanf("%f", &diagMenor);
+
+area = (diagMaior * diagMenor) / 2;
+
+printf("A área do losango é: %.2f", area);
+
+}
