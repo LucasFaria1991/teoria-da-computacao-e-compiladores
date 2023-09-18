69374555124469f5dc02c25a5b651e502ceaf3a8
LucasFaria1991 <142852119+LucasFaria1991@users.noreply.github.com>
2023-09-18 19:33:40 -0300

    Update potencia.h

--- Recursividade/potencia.h
+++ Recursividade/potencia.h
@@ -2,8 +2,8 @@ int potencia (int base, int expoente){
     if (expoente == 0 ){
         return 1;
     }else if(expoente == 1){
-    return base;
+        return base;
     }else{
-    return base * potencia(base, expoente - 1);
+        return base * potencia(base, expoente - 1);
     }
 }
